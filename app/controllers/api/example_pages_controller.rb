806cf2625d56c0e46116985782f7af9938483f37
Brian <riceb53@gmail.com>
2019-02-24 10:53:43 -0600

    add two additional routes

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -2,4 +2,12 @@ class Api::ExamplePagesController < ApplicationController
   def hello_method
     render json: {message: 'helllo'}
   end
+
+  def billy
+    render json: {bill: "bobert"}
+  end
+
+  def nashville
+    render json: {grand: "ole opry"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   get '/hello_url' => 'api/example_pages#hello_method'
+  get '/beans' => 'api/example_pages#billy'
+  get '/city' => 'api/example_pages#nashville'
 end
